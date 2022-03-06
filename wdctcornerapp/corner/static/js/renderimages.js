d24580c93c3977a36ba54346a7ba35f3274a25ca
rishav-jha-mech <rishav.rj74@gmail.com>
2022-03-06 16:16:10 +0530

    Final Changes

---
+++ wdctcornerapp/corner/static/js/renderimages.js
@@ -0,0 +1,463 @@
+
+const arya = [
+  {
+    img: "4Amit Kumar.jpg",
+    name: "Amit Kumar",
+    insta: "I_am_amit124421",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "spandan.jpeg",
+    name: "Spandan Pal",
+    insta: "spandanpal",
+    college: "National Institute of Technology Durgapur",
+    aqua: true,
+  },
+  {
+    img: "Anushka Mandalpa2.jpeg",
+    name: "Anushka Mandal",
+    insta: "mandal_anushka",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Rajeev Yadav5.jpg",
+    name: "Rajeev Yadav",
+    insta: "rajeevkumaryadav660",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Satyaki Chaudhuri1.jpg",
+    name: "Satyaki Chaudhuri",
+    insta: "sat__chan",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "ALAUKIK TIWARI.jpg",
+    name: "Alaukik Tiwari",
+    insta: "tiwari.alaukik",
+    college: "National Institute of Technology Durgapur",
+    aqua: true,
+  },
+  {
+    img: "Avinash Y.jpg",
+    name: "Avinash Yeddu",
+    insta: "_y.avinash_",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Retuja Chatterjee.png",
+    name: "Retuja Chatterjee",
+    insta: "ricotareels",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Satyaki Chaudhuri2.jpg",
+    name: "Satyaki Chaudhuri",
+    insta: "sat__chan",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "ALAUKIK TIWARI1.jpg",
+    name: "Alaukik Tiwari",
+    insta: "tiwari.alaukik",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Avinash Y2.jpg",
+    name: "Avinash Yeddu",
+    insta: "_y.avinash_",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Retuja Chatterjee1.jpg",
+    name: "Retuja Chatterjee",
+    insta: "ricotareels",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Satyaki Chaudhuri3.jpg",
+    name: "Satyaki Chaudhuri",
+    insta: "sat__chan",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "ALAUKIK TIWARI2.jpg",
+    name: "Alaukik Tiwari",
+    insta: "tiwari.alaukik",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Avinash Y3.jpg",
+    name: "Avinash Yeddu",
+    insta: "_y.avinash_",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Retuja Chatterjee2.jpg",
+    name: "Retuja Chatterjee",
+    insta: "ricotareels",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Satyaki Chaudhuri4.jpg",
+    name: "Satyaki Chaudhuri",
+    insta: "sat__chan",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Aditya Mohapatra.jpg",
+    name: "Aditya Mohapatra",
+    insta: "adimo_18",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Avinash Y4.jpg",
+    name: "Avinash Yeddu",
+    insta: "_y.avinash_",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Retuja Chatterjee3.jpg",
+    name: "Retuja Chatterjee",
+    insta: "ricotareels",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Shreya Maithani.jpg",
+    name: "Shreya Maithani",
+    insta: "shreyamaithani14",
+    college: "Doon university, Dehradun",
+    aqua: true
+  },
+  {
+    img: "Aditya Mohapatra6.jpg",
+    name: "Aditya Mohapatra",
+    insta: "adimo_18",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Avinash Y5.jpg",
+    name: "Avinash Yeddu",
+    insta: "_y.avinash_",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Retuja Chatterjee4.jpg",
+    name: "Retuja Chatterjee",
+    insta: "ricotareels",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Shreya Maithani1.jpg",
+    name: "Shreya Maithani",
+    insta: "shreyamaithani14",
+    college: "Doon university, Dehradun",
+  },
+  {
+    img: "Amit Kumar.jpg",
+    name: "Amit Kumar",
+    insta: "I_am_amit124421",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Flame 2 - Aditya Mohapatra.jpg",
+    name: "Aditya Mohapatra",
+    insta: "adimo_18",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Riddhi Chowdhury1.jpg",
+    name: "Riddhi Chowdhury",
+    insta: "meraki_latibule",
+    college: "National Institute of Technology Durgapur",
+    aqua: true
+  },
+  {
+    img: "Shreya Maithani2.jpg",
+    name: "Shreya Maithani",
+    insta: "shreyamaithani14",
+    college: "Doon university, Dehradun",
+  },
+  {
+    img: "Amit Kumar1.jpg",
+    name: "Amit Kumar",
+    insta: "I_am_amit124421",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Kiran Bolagani.JPG",
+    name: "Bolagani Kiran",
+    insta: "Kirans_art_gallary",
+    college: "Sree Dattha College of Engineering and Science",
+  },
+  {
+    img: "Riddhi Chowdhury2.png",
+    name: "Riddhi Chowdhury",
+    insta: "meraki_latibule",
+    college: "National Institute of Technology Durgapur",
+    aqua: true,
+  },
+  {
+    img: "Somwrik Dubey1.jpg",
+    name: "Somwrik Dubey",
+    insta: "somwrik.psd",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Aniket Kamble1.jpg",
+    name: "Aniket Kamble",
+    insta: "aniketkamble9192",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Kiran Bolagani2.jpg",
+    name: "Bolagani Kiran",
+    insta: "Kirans_art_gallary",
+    college: "Sree Dattha College of Engineering and Science",
+  },
+  {
+    img: "Riddhi Chowdhury3.jpg",
+    name: "Riddhi Chowdhury",
+    insta: "meraki_latibule",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Somwrik Dubey1.png",
+    name: "Somwrik Dubey",
+    insta: "somwrik.psd",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Aniket Kamble1.webp",
+    name: "Aniket Kamble",
+    insta: "aniketkamble9192",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Kiran Bolagani3.jpeg",
+    name: "Bolagani Kiran",
+    insta: "Kirans_art_gallary",
+    college: "Sree Dattha College of Engineering and Science",
+  },
+  {
+    img: "Riddhi Chowdhury4.jpg",
+    name: "Riddhi Chowdhury",
+    insta: "meraki_latibule",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Somwrik Dubey2.jpg",
+    name: "Somwrik Dubey",
+    insta: "somwrik.psd",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Aniket Kamble2.jpg",
+    name: "Aniket Kamble",
+    insta: "aniketkamble9192",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Nilashis Mukherjee.jpg",
+    name: "Nilashis Mukherjee",
+    insta: "nilashism",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Riddhi Chowdhury5.jpg",
+    name: "Riddhi Chowdhury",
+    insta: "meraki_latibule",
+    college: "National Institute of Technology Durgapur",
+    aqua: true,
+  },
+  {
+    img: "Sourav Mohanta.pdf",
+    name: "Sourav Mohanta",
+    insta: "souravmohanta4428",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Aniket Kamble3.jpg",
+    name: "Aniket Kamble",
+    insta: "aniketkamble9192",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Nilashis Mukherjee0.jpg",
+    name: "Nilashis Mukherjee",
+    insta: "nilashism",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Rishi Marodia.jpg",
+    name: "Rishi Marodia",
+    insta: "rishi_marodia_026",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Sunrise - Aditya Mohapatra.jpg",
+    name: "Aditya Mohapatra",
+    insta: "adimo_18",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Anushka Mandal1.jpg",
+    name: "Anushka Mandal",
+    insta: "mandal_anushka",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Rajeev Yadav.jpg",
+    name: "Rajeev Yadav",
+    insta: "rajeevkumaryadav660",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Rishi Marodia1.jpg",
+    name: "Rishi Marodia",
+    insta: "rishi_marodia_026",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "TAMANNA AILANI.jpeg",
+    name: "TAMANNA AILANI",
+    insta: "tamanna.ailani",
+    college: "Vivekanand Education Society Institute of Management Studies & Research",
+  },
+  {
+    img: "Anushka Mandal2.jpg",
+    name: "Anushka Mandal",
+    insta: "mandal_anushka",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Rajeev Yadav1.jpg",
+    name: "Rajeev Yadav",
+    insta: "rajeevkumaryadav660",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Rishi Marodia3.jpg",
+    name: "Rishi Marodia",
+    insta: "rishi_marodia_026",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "kiran Bolagani5.jpg",
+    name: "Bolagani Kiran",
+    insta: "Kirans_art_gallary",
+    college: "Sree Dattha College of Engineering and Science",
+  },
+  {
+    img: "Anushka Mandal3.jpeg",
+    name: "Anushka Mandal",
+    insta: "mandal_anushka",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Rajeev Yadav2.jpg",
+    name: "Rajeev Yadav",
+    insta: "rajeevkumaryadav660",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Rishi Marodia4.jpg",
+    name: "Rishi Marodia",
+    insta: "rishi_marodia_026",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "meghna maddi.jpeg",
+    name: "Meghna Maddi",
+    insta: "Meghnamaddi",
+    college: "Andhra university",
+  },
+  {
+    img: "Anushka Mandalpa1.jpeg",
+    name: "Anushka Mandal",
+    insta: "mandal_anushka",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Rajeev Yadav3.jpg",
+    name: "Rajeev Yadav",
+    insta: "rajeevkumaryadav660",
+    college: "National Institute of Technology Durgapur",
+  },
+  {
+    img: "Rishi Marodia6.jpg",
+    name: "Rishi Marodia",
+    insta: "rishi_marodia_026",
+    college: "National Institute of Technology Durgapur",
+  },
+];
+
+const slide = document.getElementById("genuine_slide");
+const Thecontent = document.getElementById("genuine_content");
+
+arya.map((content, index) => {
+  const divContent = document.createElement("div");
+  const divSlide = document.createElement("div");
+  const insta_url = `https://www.instagram.com/${content.insta}`;
+
+  const img_path = `static/all_images/${content.img}`;
+  const name = content.name;
+
+  divSlide.className = "slide";
+  divContent.className = "content__item";
+
+  divSlide.innerHTML = `
+        <div class="slide__img-wrap">
+            <div
+                class="slide__img"
+                style="background-image: url('${img_path}')">
+            </div>
+        </div>
+    <div class="slide__side">@${content.insta}</div>
+    <div class="slide__title-wrap">
+        <span class="slide__number" ${(content.aqua !== undefined) ? 'style="color: aqua;"' : ''}>${index + 1}</span>
+        <h3 class="slide__title" ${(content.aqua !== undefined) ? 'style="color: aqua;"' : ''}>${name}</h3>
+        <h4 class="slide__subtitle" ${(content.aqua !== undefined) ? 'style="color: aqua;"' : ''}>${content.college}</h4>
+    </div>
+    `;
+
+  divContent.innerHTML = `
+    <span class="content__number">${index + 1}</span>
+    <h3 class="content__title">${name}</h3>
+    <h4 class="content__subtitle">${content.college} <br> <i class="fa-brands fa-instagram" onclick="window.location.href='${insta_url}'"></i></h4>
+    <div class="content__text">
+      <button class="btn" onclick="modaltoggle2('${img_path}')">Show Art</button>
+    </div>
+    `;
+
+  slide.appendChild(divSlide);
+  Thecontent.appendChild(divContent);
+});
+
+// I know at this moment how this code is working, the only thing you need to dois to arrange the divs and elements in right order and thats what we are going todo
+
+// After the above cod has run append buttons
+const btn1 = document.createElement("button");
+const btn2 = document.createElement("button");
+const btn3 = document.createElement("button");
+btn1.className = "nav nav--prev";
+btn2.className = "nav nav--next";
+btn1.innerHTML = `
+    <svg class="icon icon--navarrow-prev">
+    <use xlink:href="#icon-navarrow"></use>
+    </svg>`;
+btn2.innerHTML = `
+    <svg class="icon icon--navarrow-next">
+    <use xlink:href="#icon-navarrow"></use>
+    </svg>
+    `;
+slide.appendChild(btn1);
+slide.appendChild(btn2);
+
+btn3.className = "content__close";
+btn3.innerHTML = `
+    <svg class="icon icon--longarrow">
+        <use xlink:href="#icon-longarrow"></use>
+    </svg>
+`;
+Thecontent.appendChild(btn3);
